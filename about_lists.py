217e489e5abfc554e645c333587695d6322a6b89
Kurta 17 <thinkers.struggle@gmail.com>
2023-09-19 14:12:07 +0700

    added

--- about_lists.py
+++ about_lists.py
@@ -10,7 +10,7 @@ def is_leap(year):
 
 
 # Task 3. You are given a dictionary with vehicles and their weights:
-vehicles = {'sedan': 1550, 'Pickup': 2000, 'bicycle': 20, 'TRUCK': 7000, 'motorcycle': 200, 'Minivan': 1700, 'SUV': 2500, 'van': 3500, 'Semi': 12000, 'Bus': 3000}
+vehicles = {'sedan': 1550, 'Pickup': 2, 'bicycle': 46570, 'TRUCK': 7000, 'motorcycle': 2, 'Minivan': 1700, 'SUV': 2500, 'van': 3500, 'Semi': 12000, 'Bus': 3000}
 
 # With single list comprehension select vehicle those weight is below 5000 kg
 # Convert names to upper-case within same list comprehension
