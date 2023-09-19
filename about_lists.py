acc8985ab3fde1b7669d8999610895b079d428ff
Chonnakarn Rungjaroenporn <chonnak.rung@gmail.com>
2023-09-19 15:24:21 +0700

    list assignment task revised

--- about_lists.py
+++ about_lists.py
@@ -1,10 +1,6 @@
 # Task 1. Create a list containin"g squares for number from 1 up to 10.
 # Use list comprehension
-sq_list = []
-
-for i in range(1, 10):
-    sq_list.append(i * i)
-
+sq_list = [i*i for i in range(1, 10) if True]
 print(sq_list)
 
 # Task 2. Create a list containig numbers leap years in the future from 2000 up to 2100
@@ -16,10 +12,13 @@ def is_leap(year):
         return True
     return False
 
-print(is_leap(2027))
+leap_list = [i for i in range(2000,2100) if is_leap(i)]
+print(leap_list[:5])
 
 # Task 3. You are given a dictionary with vehicles and their weights:
 vehicles = {'sedan': 1550, 'Pickup': 2000, 'bicycle': 20, 'TRUCK': 7000, 'motorcycle': 200, 'Minivan': 1700, 'SUV': 2500, 'van': 3500, 'Semi': 12000, 'Bus': 3000}
 
+vehicle_list = [i for i in vehicles if vehicles[i] < 5000]
+print(vehicle_list)
 # With single list comprehension select vehicle those weight is below 5000 kg
 # Convert names to upper-case within same list comprehension
