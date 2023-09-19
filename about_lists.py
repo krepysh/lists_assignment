367134700986b8df41f191803e2e7066be4a67cc
Mate <matemateberishvili@gmail.com>
2023-09-19 15:31:32 +0700

    about_lists.py

--- about_lists.py
+++ about_lists.py
@@ -1,16 +1,32 @@
 # Task 1. Create a list containing squares for number from 1 up to 10.
 # Use list comprehension
-
+array = [i**2 for i in range(1,11)]
+print(array)
 # Task 2. Create a list containig numbers leap years in the future from 2000 up to 2100
 # Use list comprehension with if condition
 # Hint: leap year is a year divisible by 4, but not by 100, unless it is divisible by 400
 
 def is_leap(year):
-    return True
+    if year%4:
+        return False
+    else:
+        if year%100:
+            return True
+        else:
+            if year%400:
+                return False
+            else:
+                return True
+
+
+[print(f"{year} - Is Leap Year") if is_leap(year) else print(f"{year} - Is not a Leap Year") for year in range(2000,2101)]
+
 
 
 # Task 3. You are given a dictionary with vehicles and their weights:
 vehicles = {'sedan': 1550, 'Pickup': 2000, 'bicycle': 20, 'TRUCK': 7000, 'motorcycle': 200, 'Minivan': 1700, 'SUV': 2500, 'van': 3500, 'Semi': 12000, 'Bus': 3000}
+[print(name) for name in vehicles if vehicles[name] < 5000]
+
 
 # With single list comprehension select vehicle those weight is below 5000 kg
 # Convert names to upper-case within same list comprehension
